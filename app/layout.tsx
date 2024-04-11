9d336fa1b9dfba9e88bf699fe61e6db46a5f10f2
gauravraj0510 <gaurav.rajghs@gmail.com>
2024-04-11 22:17:15 +0530

    final fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
         appearance={{
           layout: {
             socialButtonsVariant: "iconButton",
-            logoImageUrl: "/icons/yoom-logo.svg",
+            logoImageUrl: "/icons/logo.svg",
           },
           variables: {
             colorText: "#fff",
